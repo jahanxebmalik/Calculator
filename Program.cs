d09a24002c20342a8e665b597d7a3e8e8f000ccf
Jahanzeb Malik <jahanxeb.m@gmail.com>
2022-05-17 18:13:33 +0100

    commit

--- Program.cs
+++ Program.cs
@@ -6,7 +6,18 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to the computer program!");
+            Console.Write("Enter your first number: ");
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter your second number: ");
+            int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Choose one number option: ");
+            Console.WriteLine("1. Addition (+)");
+            Console.WriteLine("2. Subtraction (-)");
+            Console.WriteLine("3. Multiplication (*)");
+            Console.WriteLine("1. Division (/)");
         }
     }
 }
